ede4c029185ddf2774d6cef8a5fa1f6ba825270f
Russian-Dude <ghritnik@gmail.com>
2023-06-22 22:48:50 +0400

    Enable cross-origin for controller

--- backend/src/main/kotlin/com/rdude/vniizhtscreeningtask/backend/CalculatorController.kt
+++ backend/src/main/kotlin/com/rdude/vniizhtscreeningtask/backend/CalculatorController.kt
@@ -1,15 +1,13 @@
 package com.rdude.vniizhtscreeningtask.backend
 
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("api/v1/calculator")
 class CalculatorController(val calculatorService: CalculatorService) {
 
     @PostMapping
+    @CrossOrigin
     fun calculate(@RequestBody equation: String): String =
         calculatorService.calculate(equation)
 
